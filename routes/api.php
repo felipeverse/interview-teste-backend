a510538dc7a33414fff325fe135ff9a4e0eee584
felipeverse <felipealvesrrodrigues@outlook.com>
2022-04-29 12:01:07 -0400

    add user routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,116 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// CREATE A USER
+Route::post('/users/create', function(Request $request) {
+    $data = $request->all();
+
+    if (!User::where('email', '=', $data['email'])->exists()) {
+        $user = User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => Hash::make($data["password"])
+        ]);
+
+        if (empty($user->id)) {
+            return [
+                "success" => false,
+                "response" => [
+                    "error" => "An unexpected error has occured"
+                ]
+            ];
+        } else {
+            return [
+                "success" => true,
+                "response" => [
+                    "user" => $user
+                ]
+            ];
+        }
+    } else {
+        return [
+            "success" => false,
+            "response" => [
+                "error" => "The user already exists"
+            ]
+        ];
+    }
+});
+
+// GET ALL USERS
+Route::get("/users/all", function() {
+    $users = User::all();
+
+    if (empty($users)) {
+        return [
+            "success" => false,
+            "response" => [
+                "error" => "No users found"
+            ]
+        ];
+    }
+
+    return [
+        "success" => true,
+        "response" => [
+            "users" => $users
+        ]
+    ];
+});
+
+// GET A USER
+Route::get("/users/{id}", function (Request $request, $id) {
+    $user = User::find($id);
+
+    if (empty($user)) {
+        return [
+            "sucess" => false,
+            "reponse" => [
+                "error" => "No user found" 
+            ]
+        ];
+    }
+
+    return [
+        "success" => true,
+        "response" => [
+            "user" => $user
+        ]
+    ];
+});
+
+// DELETE USER
+Route::delete("/users/delete/{id}", function (Request $request, $id) {
+    $user = User::find($id);
+
+    if (empty($user))
+    {
+        $success = false;
+        $response = ["error" => "user could not be deleted."];
+    } else {
+        if ($user->delete()) {
+            $success = true;
+            $response = ["message" => "User deleted!"];
+        } else {
+            $success = false;
+            $response = ["error" => "user could not be deleted."];
+        }
+    }
+
+    return ["success" => $success, "response" => $response];
+});
+
+// UPDATE USER
+Route::put("/users/update/{id}", function (Request $request, $id) {
+    $data = $request->all();
+
+    $user = User::find($id);
+
+    foreach ($data as $key => $value) {
+        $user->{$key} = $value;
+    }
+
+    $result = $user->save();
+
+    return ["success" => $result, "response" => ["user" => $user]];
 });
